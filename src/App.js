1297f30eb76b9191794bc5f4cd18c144a5b74bac
CandiceTomkins <87971865+CandiceTomkins@users.noreply.github.com>
2022-01-02 10:25:25 +0000

    added dictionary component and search function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import "./App.css";
 import logo from "./soi55-logo.png";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} alt="logo" className="App-logo img-fluid" />
-      </header>
-      Helloooooooo
+    <div className="container">
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} alt="logo" className="App-logo img-fluid" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>coded by Candice Tomkins</small>
+        </footer>
+      </div>
     </div>
   );
 }
